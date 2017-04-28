ad9e8320be2911ade3ec6219c7939f65eaf890f6
RyanAquino <ryan_aquino004@yahoo.com>
2017-04-29 06:58:48 +0800

    added php login folder
    folder that contains admin.php, checklogin.php, index.php, connection.php, logout.php files.

---
+++ php login/admin.php
@@ -0,0 +1,14 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		<h1>This is ADMIN!</h1>
+		<a href="logout.php">Logout</a>
+	</body>
+	</html>	
---
+++ php login/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page</title>
+</head>
+<body>
+
+<form action="check_login.php" method="POST">
+	username: <input type="text" name = "username">
+	password: <input type="text" name = "password">
+			  <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php 
+//$msg=$_GET['msg']; 
+ ?>	
+</body>
+</html>

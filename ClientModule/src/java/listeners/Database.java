09dcdc94a4ec67735e59b3bbdd3257f6de432500
Patricia Mae Radie <2159296@slu.edu.ph>
2017-05-14 17:00:38 +0800

    moved updated UI to project

--- ClientModule/src/java/listeners/Database.java
+++ ClientModule/src/java/listeners/Database.java
@@ -5,35 +5,37 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import static javax.swing.JOptionPane.showMessageDialog;
+
 /**
  *
  * @author Mai Radie
  */
 public class Database {
- 
-	private Connection conn = null;
- 
-	public Database(String url, String user_name, String password) {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
- 
-			this.conn = DriverManager.getConnection(url, user_name, password);
- 
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}   
- 
-	public Connection getConnection() {
-		return this.conn;
-	}
- 
-	public ResultSet runSql(String sql) throws SQLException {
-		Statement sta = conn.createStatement();
-		return sta.executeQuery(sql);
-	}
+
+    private Connection conn = null;
+
+    public Database(String url, String user_name, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            this.conn = DriverManager.getConnection(url, user_name, password);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+
+            showMessageDialog(null, "Could not connect to database");
+
+        }
+
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    public ResultSet runSql(String sql) throws SQLException {
+        Statement sta = conn.createStatement();
+        return sta.executeQuery(sql);
+    }
 }
--- ClientModule/src/java/servlets/NotificationServlet.java
+++ ClientModule/src/java/servlets/NotificationServlet.java
@@ -11,9 +11,14 @@ import beans.Transactions;
 import java.io.IOException;
 import java.io.PrintWriter;
 import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +26,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import static javax.swing.JOptionPane.showMessageDialog;
 import listeners.Database;
 
 /**
@@ -36,24 +42,51 @@ public class NotificationServlet extends HttpServlet {
 
         HttpSession session = request.getSession(false);
 
-        // uncomment this pag may cookies na
-//        if (session == null || session.getAttribute("user") == null) {
-//            String url = response.encodeRedirectURL("NoSession.jsp");
-//            response.sendRedirect(url);
-//        } else {
-            response.setContentType("text/html");
+        response.setContentType("text/html");
 
-            Database db = (Database) getServletContext().getAttribute("db");
-            System.out.println(db);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
 
-            // change client id = 1
+            String connUrl = "jdbc:mysql://localhost:3306/webtek?user=root&password=";
+            Connection conn = DriverManager.getConnection(connUrl);
+
             String sql = "SELECT DISTINCT * FROM request NATURAL JOIN sp WHERE client_id = 1 and status = 'accepted';";
-            ResultSet rs;
-            try {
-                out.println(sql);
-                rs = db.runSql(sql);
+            Statement st = conn.createStatement();
 
-                ArrayList<Requests> requests = new ArrayList<Requests>();
+            ResultSet rs = st.executeQuery(sql);
+            ArrayList<Requests> requests = new ArrayList<Requests>();
+            while (rs.next()) {
+
+                Requests req = new Requests();
+                req.setRequest_id(rs.getString("request_id"));
+                req.setSp_id(rs.getString("sp_id"));
+                req.setClient_id(rs.getString("client_id"));
+                req.setStatus(rs.getString("status"));
+                req.setDate(rs.getString("date"));
+                req.setTime(rs.getString("time"));
+                req.setSched_id(rs.getString("sched_id"));
+
+                ServiceProvider sp = new ServiceProvider();
+                sp.setSpId(rs.getString("sp_id"));
+
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            
+            request.setAttribute("requests", requests);
+            RequestDispatcher view = request.getRequestDispatcher("Notification.jsp");
+            
+            view.forward(request, response);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(NotificationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(NotificationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //   rs = db.runSql(sql);
+//        ArrayList<Requests> requests = new ArrayList<Requests>();
 
 //            Requests req = new Requests(
 //                    rs.getString("request_id"),
@@ -63,33 +96,26 @@ public class NotificationServlet extends HttpServlet {
 //                    rs.getString("date"),
 //                    rs.getString("time"),
 //                    rs.getString("sched_id"));
-                while (rs.next()) {
-
-                    Requests req = new Requests();
-                    req.setRequest_id(rs.getString("request_id"));
-                    req.setSp_id(rs.getString("sp_id"));
-                    req.setClient_id(rs.getString("client_id"));
-                    req.setStatus(rs.getString("status"));
-                    req.setDate(rs.getString("date"));
-                    req.setTime(rs.getString("time"));
-                    req.setSched_id(rs.getString("sched_id"));
-
-                    ServiceProvider sp = new ServiceProvider();
-                    sp.setSpId(rs.getString("sp_id"));
-
-                }
-
-                request.setAttribute("requests", requests);
-                RequestDispatcher view = request.getRequestDispatcher("Notification.jsp");
-                view.forward(request, response);
+//            while (rs.next()) {
+//
+//                Requests req = new Requests();
+//                    req.setRequest_id(rs.getString("request_id"));
+//                    req.setSp_id(rs.getString("sp_id"));
+//                    req.setClient_id(rs.getString("client_id"));
+//                    req.setStatus(rs.getString("status"));
+//                    req.setDate(rs.getString("date"));
+//                    req.setTime(rs.getString("time"));
+//                    req.setSched_id(rs.getString("sched_id"));
+//
+//                    ServiceProvider sp = new ServiceProvider();
+//                    sp.setSpId(rs.getString("sp_id"));
+//
+//                }
+//            request.setAttribute("requests", requests);
+//            RequestDispatcher view = request.getRequestDispatcher("Notification.jsp");
+//            view.forward(request, response);
 //            response.sendRedirect(request.)
-
-            } catch (SQLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
+    }
 
 //    }
-
 }

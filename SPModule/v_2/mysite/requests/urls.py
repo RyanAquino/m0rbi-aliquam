0d7da5d61f4a799fda5ea9e44f0012cb088c9d28
jamesRedondo <jamesredundancy@gmail.com>
2017-05-15 20:49:09 +0800

    Added database backup tutorial_qwerty.sql

--- SPModule/v_2/mysite/requests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    #/requests/
-    url(r'^$', views.index, name='index'),
-    #/requests/verdict
-    url(r'^verdict/(?P<request_id>[0-9]+)', views.verdict, name='verdict'),
-]
